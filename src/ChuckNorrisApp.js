2d1034cd2ed43d51f0ee67e69e9105a5cef4a77f
Sander Vink <sander.vink@procurios.nl>
2017-06-23 22:51:07 +0200

    Add/remove favourite jokes from local storage

--- src/ChuckNorrisApp.js
+++ src/ChuckNorrisApp.js
@@ -10,17 +10,47 @@ class ChuckNorrisApp extends Component {
         };
 
         this.favouriteJokes = new Map();
+        this.localStorageKey = 'myFavouriteChuckNorrisJokes';
+        this.maximumFavouriteJokesAllowed = 3;
+
+        // Check for locally stored favourite jokes
+        if (localStorage.getItem(this.localStorageKey)) {
+            this.favouriteJokes = new Map(JSON.parse(localStorage.getItem(this.localStorageKey)));
+        }
 
         this.handleAddToFavourites = this.handleAddToFavourites.bind(this);
         this.handleRemoveFromFavourites = this.handleRemoveFromFavourites.bind(this);
     }
 
     handleAddToFavourites(joke) {
-        alert('Todo - Add favourite joke!');
+        // Prevent adding a new favourite joke when the limit is reached
+        if (this.favouriteJokes.size >= this.maximumFavouriteJokesAllowed) {
+            alert(`You are not allowed to have more than ${this.maximumFavouriteJokesAllowed} favourite jokes!`);
+            return;
+        }
+
+        // Mark the joke as favourite, and save it to local storage
+        joke.isFavourite = true;
+        this.favouriteJokes.set(joke.id, joke);
+        localStorage.setItem(this.localStorageKey, JSON.stringify(Array.from(this.favouriteJokes.entries())));
+
+        // Set state to update the lists
+        this.setState({
+            addFavouriteJoke: joke.id,
+            removeFavouriteJoke: false
+        });
     }
 
     handleRemoveFromFavourites(joke) {
-        alert('Todo - Remove favourite joke!');
+        // This joke is no longer a favourite, and should be removed from local storage
+        this.favouriteJokes.delete(joke.id);
+        localStorage.setItem(this.localStorageKey, JSON.stringify(Array.from(this.favouriteJokes.entries())));
+
+        // Set state to update the lists
+        this.setState({
+            addFavouriteJoke: false,
+            removeFavouriteJoke: joke.id
+        });
     }
 
     render() {
